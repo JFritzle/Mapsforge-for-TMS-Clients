48a4edd602d93a89d04954b7bdde44f3e6e07237
JFritzle <62614244+JFritzle@users.noreply.github.com>
2023-09-05 10:11:41 +0200

    Update files via upload

--- Mapsforge-for-TMS-Clients.tcl
+++ Mapsforge-for-TMS-Clients.tcl
@@ -431,12 +431,13 @@ set maps [lsort -dictionary $maps]
 if {[llength $maps] == 0} {error_message [mc e11] exit}
 
 # Get list of available Mapsforge themes
-# and add Mapsforge built-in default theme
+# and add Mapsforge built-in themes
 
 cd $themes_folder
 set themes [find_files "" "*.xml"]
 cd $cwd
-lappend themes "(default)"
+if {$::server_version <  2000} {lappend themes "(default)"}
+if {$::server_version >= 2000} {lappend themes "(DEFAULT)" "(OSMARENDER)"}
 set themes [lsort -dictionary $themes]
 
 # --- Begin of main window
@@ -1143,8 +1144,8 @@ proc setup_styles_overlays_structure {} {
     foreach child [winfo children .overlays] {destroy $child}
   }
 
-  # Default theme has no style: nothing to do
-  # Default theme has hillshading: enable hillshading configuration
+  # Built-in themes have no style: nothing to do
+  # Built-in themes have hillshading: enable hillshading configuration
   set theme ${::theme.selection}
   if {$theme == "(default)"} {
     unset -nocomplain ::style.table ::style.theme
@@ -1675,7 +1676,9 @@ proc srv_start {srv} {
     lappend command -m [join $map_list ","]
     if {${::maps.world} == 1} {lappend command -wm}
     set theme [.themes_values get]
-    if {$theme != "(default)"} {
+    if {$theme == "(DEFAULT)" || $theme == "(OSMARENDER)"} {
+      lappend command -t [string trim $theme ()]
+    } elseif {$theme != "(default)"} {
       set theme_file "$::themes_folder/$theme"
       lappend command -t $theme_file
       if {[winfo manager .styles] != ""} {
